afbd941966701f90dab918b673a46c7e6e5abd99
Mikemiller34 <71727845+Mikemiller34@users.noreply.github.com>
2020-12-28 15:04:31 -0500

    Update Enable LSA Protection.ps1

---
+++ Security Scripts/Enable LSA Protection.ps1
@@ -0,0 +1,11 @@
+Enable LSA Protection
+
+Create Registry Key:
+HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa
+Name: RunAsPPL
+Type: DWord
+Value: 1
+
+Script:
+Set-Locations HKLM:
+New-Item -Path .\SYSTEM\CurrentControlSet\Control\LSA -Name RunAsPPL -Value 1 -Type DWord
