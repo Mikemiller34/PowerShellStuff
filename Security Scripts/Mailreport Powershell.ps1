7af4c186ef839d82ab79bc275409717f5e200bec
Mikemiller34 <71727845+Mikemiller34@users.noreply.github.com>
2021-01-03 20:45:01 -0500

    Update Mailreport Powershell.ps1

--- Security Scripts/Mailreport Powershell.ps1
+++ Security Scripts/Mailreport Powershell.ps1
@@ -4,3 +4,6 @@ Get-MailflowStatusReport -StartDate 09-01-2020 -EndDate 09-30-2020 -Direction In
 Get-MailflowStatusReport -StartDate 09-01-2020 -EndDate 09-30-2020 -Direction Inbound -EventType EdgeBlockSpam,SpamDetections | Format-Table MessageCount | Out-File "C:\Users\Mike\Desktop\Spam.csv"
 Get-MailflowStatusReport -StartDate 09-01-2020 -EndDate 09-30-2020 -Direction Inbound -EventType EmailPhish | Format-Table MessageCount | Out-File "C:\Users\Mike\Desktop\Phish.csv"
 Get-MailflowStatusReport -StartDate 09-01-2020 -EndDate 09-30-2020 -Direction Inbound -EventType EmailMalware | Format-Table MessageCount | Out-File "C:\Users\Mike\Desktop\Malware.csv"
+
+<!-- Quarantine report: Run for page 1, then 2, then 3, and so on until you stop getting 1000 results per page, then add them up and you have your monthly count. -->
+Get-QuarantineMessage -StartReceivedDate 12/01/2020 -EndReceivedDate 01/01/2021 -PageSize 1000 -Page 1| Out-File "C:\Users\Mike\Desktop\SentToQuarantineDec.csv"
