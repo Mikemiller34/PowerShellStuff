5a1d5b427892aa57067eea5da634aea57aed8ac0
Mikemiller34 <71727845+Mikemiller34@users.noreply.github.com>
2021-04-23 09:51:07 -0400

    Create Azure AD User Management.ps1

---
+++ Azure AD User Management.ps1
@@ -0,0 +1,13 @@
+>> Remove Azure AD User
+
+    >> Connect-AzureAD
+
+    >> Remove-AzureADUser -ObjectId "Users ObjectId"
+
+>> Create Azure AD User
+
+    >> New-AzureADUser
+
+    >> See different properties here:
+    https://docs.microsoft.com/en-us/powershell/module/azuread/new-azureaduser?view=azureadps-2.0
+
