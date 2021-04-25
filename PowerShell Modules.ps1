063813e2ade1e8c4c055d7528f72d20e1474e0aa
Mikemiller34 <71727845+Mikemiller34@users.noreply.github.com>
2021-04-25 11:21:05 -0400

    Create PowerShell Modules.ps1

---
+++ PowerShell Modules.ps1
@@ -0,0 +1,35 @@
+>> Exchange Online Management
+
+    >> Install-Module ExchangeOnlineManagement
+
+    >> Connect-ExchangeOnline
+
+>> azuread
+
+    >> Install-Module AzureAD
+
+    >> Connect-AzureAD
+
+>> Msonline
+
+    >> Install-Module Msonline
+
+    >> Connect-MsolService
+
+>> Microsoft Teams
+
+    >> Install-Module MicrosoftTeams
+
+    >> Connect-MicrosoftTeams
+
+>> SharePoint Online
+
+    >> Install-Module Microsoft.Online.SharePoint.PowerShell
+
+    >> Connect-SPOService
+
+>> Security and Compliance Center
+
+    >> No install needed
+
+    >> Connect-IPPSSession
