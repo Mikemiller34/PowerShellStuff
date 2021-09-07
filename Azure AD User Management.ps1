cc658c8c5f6d9bda035679ef4dca4605b4e8bd69
Mikemiller34 <71727845+Mikemiller34@users.noreply.github.com>
2021-09-07 08:57:00 -0400

    Update Azure AD User Management.ps1

--- Azure AD User Management.ps1
+++ Azure AD User Management.ps1
@@ -8,6 +8,15 @@
 
     >> New-AzureADUser
 
+        New-MsolUser -UserPrincipalName "UPN" -DisplayName "Name" -FirstName "FirstName" -LastName "LastName" -Department "Department"
+
+
+
+    New-MsolGroup -DisplayName "Group Name" -Description "Group Description"
+
+
+New-UnifiedGroup -DisplayName "Name" -Alias "Alias" -EmailAddresses "Email" -AccessType Public/Private
+
     >> See different properties here:
     https://docs.microsoft.com/en-us/powershell/module/azuread/new-azureaduser?view=azureadps-2.0
 
