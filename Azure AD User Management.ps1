47a36adc9af7b9c0026814504fa6f30936dffa13
Mikemiller34 <71727845+Mikemiller34@users.noreply.github.com>
2021-10-01 08:13:47 -0400

    Update Azure AD User Management.ps1

--- Azure AD User Management.ps1
+++ Azure AD User Management.ps1
@@ -1,22 +1,38 @@
 >> Remove Azure AD User
 
-    >> Connect-AzureAD
+>> Connect-AzureAD
 
-    >> Remove-AzureADUser -ObjectId "Users ObjectId"
+>> Remove-AzureADUser -ObjectId "Users ObjectId"
 
 >> Create Azure AD User
 
-    >> New-AzureADUser
+>> New-AzureADUser
 
-        New-MsolUser -UserPrincipalName "UPN" -DisplayName "Name" -FirstName "FirstName" -LastName "LastName" -Department "Department"
+New-MsolUser -UserPrincipalName "UPN" -DisplayName "Name" -FirstName "FirstName" -LastName "LastName" -Department "Department"
 
 
 
-    New-MsolGroup -DisplayName "Group Name" -Description "Group Description"
+New-MsolGroup -DisplayName "Group Name" -Description "Group Description"
 
 
 New-UnifiedGroup -DisplayName "Name" -Alias "Alias" -EmailAddresses "Email" -AccessType Public/Private
 
-    >> See different properties here:
-    https://docs.microsoft.com/en-us/powershell/module/azuread/new-azureaduser?view=azureadps-2.0
+>> See different properties here:
+https://docs.microsoft.com/en-us/powershell/module/azuread/new-azureaduser?view=azureadps-2.0
 
+Get User count
+
+# of users DirSync
+    (Get-AzureADUser -all $true | Where { $_.DirSyncEnabled -eq $true }).Count
+
+List of DirSync users
+    (Get-AzureADUser -all $true | Where { $_.DirSyncEnabled -eq $true }
+
+# of cloud only
+    (Get-AzureADUser -all $true | Where { $_.DirSyncEnabled -eq $null }).Count
+
+List of cloud only
+     (Get-AzureADUser -all $true | Where { $_.DirSyncEnabled -eq $null }
+
+
+* * * Use Get-AzureADGroup for number of groups instead of users***
