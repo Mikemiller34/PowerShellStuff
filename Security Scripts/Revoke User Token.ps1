3bce0928f2feaf9c05bb3445d62741eb3f691f4f
Mikemiller34 <71727845+Mikemiller34@users.noreply.github.com>
2021-12-21 19:07:08 -0500

    Update Revoke User Token.ps1

--- Security Scripts/Revoke User Token.ps1
+++ Security Scripts/Revoke User Token.ps1
@@ -11,7 +11,3 @@ Get-AzureADUser -ALL 1 | Revoke-AzureADUserAllRefreshToken
 Revoke tokens for single employee
 Connect-AzureAD
 Get-AzureADUser -ObjectId (Paste Object ID found on user's page in Azure AD>Users) | Revoke-AzureADUserAllRefreshToken
-
-Example: (Wizard account)
-Connect-AzureAD
-Get-AzureADUser -ObjectId 98d93f11-c319-4f71-bca6-a950fa500ec1 | Revoke-AzureADUserAllRefreshToken
